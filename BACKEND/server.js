a5caf4382628171e115256cfb0d2d7ea6340b0f3
Kajal Pandit <kajalpandit830@gmail.com>
2026-02-23 18:03:20 +0530

    Initial commit

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -5,17 +5,14 @@ import cookieParser from "cookie-parser";
 import connectDB from "./Config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
-import path from "path";
-import { fileURLToPath } from "url";
+
 
 
 
 const app = express();
 const PORT = process.env.PORT || 3000
 connectDB();
-const allowedOrigins = ['http://localhost:5173','https://mern-auth-zzdt.onrender.com']
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const allowedOrigins = ['http://localhost:5173']
 
 
 app.use(express.json());    //to parse every request
@@ -31,12 +28,6 @@ app.get('/', (req, res) => {
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 
-// Serve frontend build
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
 
 app.listen(PORT, () =>{
     console.log(`Server started on port : ${PORT}`);
