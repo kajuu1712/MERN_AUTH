32c23003d537bc2f20b410a2ae73667b9e581552
Kajal Pandit <kajalpandit830@gmail.com>
2026-02-23 19:07:53 +0530

    Frontend changes

--- FRONTEND/src/components/Navbar.jsx
+++ FRONTEND/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   const sendVerificationOtp = async() => {
     try {
       axios.defaults.withCredentials = true
-      const {data} = await axios.post(backendUrl + '/api/auth/send-verify-otp')
+      const {data} = await axios.post(`${backendUrl}/api/auth/send-verify-otp`)
       if(data.success){
         navigate('/email-verify')
         toast.success(data.message)
@@ -26,7 +26,7 @@ const Navbar = () => {
   const logout = async() => {
     try {
       axios.defaults.withCredentials = true
-      const {data} = await axios.post(backendUrl + '/api/auth/logout')
+      const {data} = await axios.post(`${backendUrl}/api/auth/logout'`)
       data.success && setIsLoggedin(false)
       data.success && setUserData(false)
       navigate('/')
