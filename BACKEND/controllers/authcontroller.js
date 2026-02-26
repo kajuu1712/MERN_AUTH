6dd2e5e1b496a4907adfb9ed80b23a11f4072f8c
Kajal Pandit <kajalpandit830@gmail.com>
2026-02-26 14:04:55 +0530

    server.js changes

--- BACKEND/controllers/authcontroller.js
+++ BACKEND/controllers/authcontroller.js
@@ -28,8 +28,8 @@ export const register = async (req, res) => {
         const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', 
+            secure: true,
+            sameSite: none, 
             maxAge: 7 * 24 * 60 * 60 * 1000
         }) 
 
@@ -95,8 +95,8 @@ export const logout = async (req, res) => {
     try{
         res.clearCookie('token', {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', 
+            secure: true,
+            sameSite: none, 
         })
 
         return res.json({success: true, message: "Logged Out"});
