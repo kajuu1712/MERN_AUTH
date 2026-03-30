39ac14beed5029eda49e37c6061d01697684dc76
Kajal Pandit <kajalpandit830@gmail.com>
2026-03-30 22:01:09 +0530

    Changes made

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -6,12 +6,6 @@ import connectDB from "./Config/mongodb.js";
 import authRouter from "./routes/authRoutes.js";
 import userRouter from "./routes/userRoutes.js";
 
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 const app = express();
 const PORT = process.env.PORT || 3000
 connectDB();
@@ -22,9 +16,6 @@ app.use(express.json());    //to parse every request
 app.use(cookieParser());
 app.use(cors({origin: allowedOrigins, credentials: true}));
 
-// Serve frontend
-app.use(express.static(path.join(__dirname, "../FRONTEND/dist"))); 
-
 //API End points
 app.get('/', (req, res) => {
     res.send("API working");
@@ -32,10 +23,6 @@ app.get('/', (req, res) => {
 
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
-
-app.get(/(.*)/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../FRONTEND/dist/index.html")); 
-});
 
 app.listen(PORT, () =>{
     console.log(`Server started on port : ${PORT}`);
